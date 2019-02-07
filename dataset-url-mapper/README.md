251e551593bef7ff19454ce0b386ac5ce575028c
Sasha Ames <amysash2006@gmail.com>
2019-02-07 13:24:09 -0800

    small refactor add example comments

---
+++ dataset-url-mapper/README.md
@@ -0,0 +1,4 @@
+dataset-url-mapper
+
+Examples of mapping datasets to files (this procedure retrieves file urls and converts to physical paths)
+
--- dataset-url-mapper/map_files.py
+++ dataset-url-mapper/map_files.py
@@ -2,7 +2,10 @@ import requests, json
 from urlparse import urlparse
 
 # Load this table from a .json file
-table = { 'esg_dataroot' : '/path/to/data' }
+sample_table = { 'esg_dataroot' : '/path/to/data',
+		 '/cmip5_css02_data': '/path/to/cmip5_02/data',
+		 '/cmip5_css01_data': '/path/to/cmip5_01/data',
+		  'css03_data': '/path/to/css03/data', 'user_pub_work': '/path/to/user/data' }
 
 
 
@@ -19,7 +22,7 @@ def get_mapped_dataset(id):
 	return urllst
 
 
-def parse_and_map(url):
+def parse_and_map(url, table):
 
 	strparts = url.split('|')
 	res = urlparse(strparts[0])
@@ -37,12 +40,14 @@ def map_datasets(id):
 
 	for url in get_mapped_dataset(id):
 
-		print parse_and_map(url)
+		print parse_and_map(url, sample_table)
 
 
 import sys
 
+# test example files:
+#  'cmip5.output1.CMCC.CMCC-CM.historical.day.atmos.day.r1i1p1.v20120514|aims3.llnl.gov'
+# 'CMIP6.CMIP.NASA-GISS.GISS-E2-1-G.historical.r1i1p1f1.3hr.clt.gn.v20181015|aims3.llnl.gov'
+
 map_datasets(sys.argv[1])
-
-
 
