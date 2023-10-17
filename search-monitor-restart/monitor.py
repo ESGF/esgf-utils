ec07c5d78bb4211c0d069f4d93aa4b237677278e
ames4 <ames4@llnl.gov>
2023-10-17 09:16:07 -0700

    inital

---
+++ search-monitor-restart/monitor.py
@@ -0,0 +1,34 @@
+import os
+import requests
+
+from time import sleep
+
+URL = "https://esgf-node.llnl.gov/esg-search/search/?limit=0&facets=index_node&format=application%2Fsolr%2Bjson"
+ANSIBLE_RESTART = "ansible-playbook -i esgf.hosts --limit esgf-node.llnl.gov stop.yml start.yml"
+
+def do_check():
+
+    resp = requests.get(URL)
+
+    print(type(resp.status_code), resp.status_code)
+
+    if resp.status_code != 200:
+        print("Need to Restart")
+#        os.system(ANSIBLE_RESTART)
+#        return
+
+
+    try:
+        jo = json.loads(resp.text)
+        facets = jo["facet_counts"]["facet_fields"]["index_node"]    
+
+        if len(facets) < 4:
+            print("Need to restart")
+            #os.system(ANSIBLE_RESTART)
+            return
+    except:
+        print("JSON Parse error or other")
+
+while (True):
+    do_check()
+    sleep(600)
